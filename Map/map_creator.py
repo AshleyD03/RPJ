ec1bcfb6f9a44a80465d5a0cc34fd7dcd88f788f
Ashley <adoel3507@gmail.com>
2020-03-06 09:48:05 +0000

    Added door/enemy/walls UI, fix door check

--- Map/map_creator.py
+++ Map/map_creator.py
@@ -9,16 +9,15 @@ for i in range(1,8):
 pos = {"x": 2,
      "y": 2}
 
-current = "o"
+current = " "
 last = " "
 
 # Colors
 col_clear = attr('reset')
 col_current = bg(196)
 
-def change_colour(colour):
-    last = lines[pos["y"]][pos["x"]]
-    lines[pos["y"]][pos["x"]] = (col_current + current + col_clear)
+def change_current(icon, colour):
+    lines[pos["y"]][pos["x"]] = (col_current + icon + col_clear)
 
 def revert(last):
     if last == "":
@@ -28,22 +27,28 @@ def revert(last):
 def move(cmd, last):
     if cmd == "w":
         if pos["y"] - 1 < 0 :
-            return()
+            return(last)
         revert(last); pos["y"] -= 1
     if cmd == "a": 
         if pos["x"] - 1 < 0 :
-            return()
+            return(last)
         revert(last); pos["x"] -= 1
     if cmd == "s":
         if pos["y"] + 1 > 7 :
-            return()
+            return(last)
         revert(last); pos["y"] += 1
     if cmd == "d": 
         if pos["x"] + 1 > 34 :
-            return()
+            return(last)
         revert(last); pos["x"] += 1
     return(lines[pos["y"]][pos["x"]])
-    
+
+def remove(symbol):
+    for y in range (8):
+        for x in range (35):
+            if symbol in lines[y][x]: 
+                lines[y][x] = " "; break
+
 def output_grid():
     print("X"*37)
     for y in range (8):
@@ -53,17 +58,68 @@ def output_grid():
         print("X")
     print("X"*37)
 
+#class Enemy():
+change_current(current, col_current)
+help_line = "Helpy: Hi there Ashley o/"
 while True:
-    change_colour(col_current)
-    output_grid()
-    entry = str(msvcrt.getch())[2]; print(entry)
+    output_grid(); print("(B) - Wall    (N) - Enemy  (M) - Spawn\n(C) - Edit    (V) - Door   (,) - Delete\n"+help_line)
+    entry = str(msvcrt.getch())[2]; 
 
     # W/A/S/D command : move commands
     if entry in ["w","a","s","d"]:
         last = move(entry, last)
+        change_current(last, col_current)
+        help_line = "Helpy: Move "+entry
 
-    # R command : output room value
-    #elif entry == "r": 
+    # Inner Rim commands check position is possible
+    elif entry in ["n","m","b"] and pos["x"] not in [0,34] and pos["y"] not in [0,7]:   
+        # add Enemy
+        if entry == "n": 
+            change_current("@", col_current); last = "@"
+            help_line = "Helpy: Enemy spawned"
+
+        # add Spawn 
+        elif entry == "m": 
+            remove("S")
+            change_current("S", col_current); last = "S"
+            help_line = "Helpy: Spawn Moved"
+
+        # add Wall
+        elif entry == "b":
+            change_current("/", col_current);  last = "/"
+            help_line = "Helpy: Added Wall"
+
+    # Inner rim used in outer check
+    elif entry in ["n","m","b"]:
+        help_line = "Helpy: Can't use these on outer rim"
+
+    # add Door
+    elif entry == "v":
+        # Check if door allready there 
+        
+        if:
+            help_line = "Helpy: Sorry door already there"
+           
+        # horizontal door
+        elif pos["y"] == 0 or pos["y"] == 7:
+            if pos["x"] > 1 and pos["x"] < 33: 
+                lines[pos["y"]][pos["x"] - 1] = "["; lines[pos["y"]][pos["x"] + 1] = "]"
+                change_current(" ", col_current); last = " "
+                help_line = "Helpy: Horizontal door added"
+            else:
+                help_line = "Helpy: Horizontal door too close to sides"
+
+        # vertical door
+        elif pos["x"] == 0 or pos["x"] == 34:
+            if pos["y"] > 1 and pos["y"] < 6:
+                lines[pos["y"] - 1][pos["x"]] = "-"; lines[pos["y"] +1][pos["x"]] = "-"
+                change_current(" ", col_current); last = " "
+                help_line = "Helpy: Vertical door added"
+            else:
+                help_line = "Helpy: Vertical door to close to sides"
+
+        else:
+            help_line = "Helpy: Sorry, doors can only go on sides"
 
     # Q command : quit command
     elif entry == "q":
